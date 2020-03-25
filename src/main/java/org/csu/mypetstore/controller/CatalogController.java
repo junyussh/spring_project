c6e13de33faf5e869dd83c567fd093e7b7580623
aczy156 <614421228@qq.com>
2020-03-25 08:17:44 +0800

    Thymeleaf 初始化

---
+++ src/main/java/org/csu/mypetstore/controller/CatalogController.java
@@ -0,0 +1,15 @@
+package org.csu.mypetstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/catalog")
+public class CatalogController {
+
+    @GetMapping("/main")
+    public String view(){
+        return "catalog/main";
+    }
+}
